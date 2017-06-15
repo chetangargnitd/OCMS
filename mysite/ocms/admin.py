c55f24a8149579a543c268bc2b0d1de91b251e87
Sahil Jain <jainsahil1997@hotmail.com>
2017-06-15 13:17:09 +0530

    Complaint generated

--- mysite/ocms/admin.py
+++ mysite/ocms/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Department, Complaint_Type, Property_Desc,Property
-#admin.site.register(Complaint)
+from .models import Complaint,Department, Complaint_Type, Property_Desc,Property
+admin.site.register(Complaint)
 admin.site.register(Department)
 admin.site.register(Complaint_Type)
 admin.site.register(Property)
--- mysite/ocms/models.py
+++ mysite/ocms/models.py
@@ -27,3 +27,26 @@ class Property_Desc(models.Model):
 
     def __str__(self):
         return '{}-{}' .format(self.property_name,self.room_name)
+
+
+class Complaint(models.Model):
+    auto_increment_id = models.AutoField(primary_key=True)
+    dd_date = models.DateTimeField(auto_now_add=True,blank=True, null=True)
+    dept_name=models.ForeignKey(Department,on_delete=models.CASCADE,blank=True, null=True)
+    complaint_type=ChainedForeignKey(Complaint_Type,
+                            chained_field="dept_name",
+                            chained_model_field="dept_name",
+                            show_all=False,
+                            auto_choose=False,
+                            sort=True,)
+    property_name=models.ForeignKey(Property,on_delete=models.CASCADE,blank=True, null=True)
+    room_name=ChainedForeignKey(Property_Desc,null=True, blank=True,
+                            chained_field="property_name",
+                            chained_model_field="property_name",
+                            show_all=False,
+                            auto_choose=False,
+                            sort=True,)
+    complaint_desc=models.CharField(max_length=1500,blank=False,null=False,default='code')
+
+    def __str__(self):
+        return '{}/{}/{}' .format(self.dept_name,self.complaint_type,self.auto_increment_id)
